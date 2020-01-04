1d687700bf1c72bb8c4b0f3795648ca22e6a8b44
zerosnake0 <zerosnake0@gmail.com>
2020-01-05 01:19:34 +0800

    improve iterator performance

--- iterator_pool.go
+++ iterator_pool.go
@@ -26,13 +26,10 @@ func NewIteratorPool() *IteratorPool {
 
 func (p *IteratorPool) BorrowIterator() *Iterator {
 	it := p.pool.Get().(*Iterator)
-	// it.tmpBuffer = getByteSlice()
 	return it
 }
 
 func (p *IteratorPool) ReturnIterator(it *Iterator) {
 	it.reset()
-	// releaseByteSlice(it.tmpBuffer)
-	// it.tmpBuffer = nil
 	p.pool.Put(it)
 }
